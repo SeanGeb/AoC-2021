27156d1a7d7ce0d4def0b0582100de4758ca45f2
Sean Gebbett <10674942+SeanGeb@users.noreply.github.com>
2021-12-02 22:45:19 +0000

    [day1] Tidy

--- day1/day1.pl
+++ day1/day1.pl
@@ -1,11 +1,11 @@
 incrs(_, [], 0).
-incrs(N, [H|T], R) :- H =< N, incrs(H, T, R).
-incrs(N, [H|T], R) :- H > N, incrs(H, T, Rn), R is Rn + 1.
+incrs(Prev, [H|T], Result) :- H =< Prev, incrs(H, T, Result).
+incrs(Prev, [H|T], Result) :- H > Prev, incrs(H, T, NextResult), Result is NextResult + 1.
 
 % Unifies a list with the number of times, for each pair of adjacent
 % elements (A, B), that B > A.
-incrs([], 0).
-incrs([H|T], R) :- incrs(H, T, R).
+solve1([], 0).
+solve1([H|T], Result) :- incrs(H, T, Result).
 
 
 incrs3(_, [], 0).
@@ -23,7 +23,7 @@ incrs3(Prev, [H1, H2, H3 | T], R) :-
 
 % Unifies a list with the number of times, for each run of four adjacent
 % elements (A, B, C, D), that B+C+D > A+B+C.
-incrs3([], 0).
-incrs3([_], 0).
-incrs3([_, _], 0).
-incrs3([H1, H2, H3 | T], R) :- N is H1 + H2 + H3, incrs3(N, [H2, H3 | T], R).
+solve2([], 0).
+solve2([_], 0).
+solve2([_, _], 0).
+solve2([H1, H2, H3 | T], R) :- N is H1 + H2 + H3, incrs3(N, [H2, H3 | T], R).
